e89c5b67175156ffb2188a43b67746092cf3f539
海洋 贾 <84194041@qq.com>
2018-10-02 23:50:14 +0800

    update

---
+++ TopShelf_WindowsService/Program.cs
@@ -0,0 +1,23 @@
+﻿using Topshelf;
+
+namespace TopShelf_WindowsService
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            HostFactory.Run(x =>
+            {
+                x.UseNLog();
+                x.Service<ServiceRunner>();
+                x.RunAsLocalSystem();
+
+                x.SetDescription("TopShelf服务简单测试");
+                x.SetDisplayName("TopShelfService");
+                x.SetServiceName("TopShelfService");//批处理文件根据这个打开和关闭服务
+
+                x.EnablePauseAndContinue();
+            });
+        }
+    }
+}
