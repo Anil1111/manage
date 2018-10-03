ee05e4e5dcf4c1b32554780d76c48a89bb27d500
海洋 贾 <84194041@qq.com>
2018-10-03 13:48:21 +0800

    update

---
+++ TopShelf_WindowsService/QuartzJobs/CancelOrderJob.cs
@@ -0,0 +1,24 @@
+﻿using log4net;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopShelf_WindowsService.QuartzJobs
+{
+    public class CancelOrderJob : IJob
+    {
+        private ILog logger;
+        public CancelOrderJob()
+        {
+            logger = LogManager.GetLogger(this.GetType());
+        }
+
+        public void Execute(IJobExecutionContext context)
+        {
+            logger.Info("取消订单");
+        }
+    }
+}
