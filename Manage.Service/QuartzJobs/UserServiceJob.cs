8429ec6f38ac6a20fe870ddd0122e6090d087827
海洋 贾 <84194041@qq.com>
2018-10-06 13:28:55 +0800

    update

---
+++ Manage.Service/QuartzJobs/UserServiceJob.cs
@@ -0,0 +1,32 @@
+﻿using Manage.Data;
+using Manage.Data.Data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manage.Service.QuartzJobs
+{
+    public class UserServiceJob
+    {
+        private static ManageDBEntities context;
+        public UserServiceJob()
+        {
+            context = new ManageDBEntities();
+        }
+
+        public List<Sys_User> Entities(string userName)
+        {
+            string sql = "select * from Sys_User a where a.UserName = @0";
+            SqlParameter[] parameter = new SqlParameter[] {
+                new SqlParameter("@0", SqlDbType.VarChar, 50)
+            };
+            parameter[0].Value = userName;
+
+            return context.Database.SqlQuery<Sys_User>(sql, parameter).ToList();
+        }
+    }
+}
--- TopShelf_WindowsService/QuartzJobs/TestJob.cs
+++ TopShelf_WindowsService/QuartzJobs/TestJob.cs
@@ -1,20 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
 using log4net;
+using Manage.Core.Json;
+using Manage.Data;
+using Manage.Service.QuartzJobs;
 using Quartz;
 
 namespace TopShelf_WindowsService.QuartzJobs
 {
     public sealed class TestJob : IJob
     {
-        private ILog logger;
+        private ILog _logger;
+        private UserServiceJob _userServiceJob;
         public TestJob()
         {
-            logger = LogManager.GetLogger(this.GetType());
+            _logger = LogManager.GetLogger(this.GetType());
+            _userServiceJob = new UserServiceJob();
         }
 
         public void Execute(IJobExecutionContext context)
         {
-            logger.Info("TestJob测试");
+            try
+            {
+                List<Sys_User> list = this._userServiceJob.Entities("admin");
+                string jsonStr = JsonUtil.SerializerObject(list);
+                _logger.Info(jsonStr);
+            }
+            catch (Exception ex)
+            {
+                _logger.Info(ex.Message);
+            }
         }
     }
 }
